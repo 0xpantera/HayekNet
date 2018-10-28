4e2e513aef653d2f87ad5275da1c817c21aee96b
Francososa <franky.sosa@gmail.com>
2018-10-28 13:53:06 -0600

    Define initial Tensor class.

---
+++ tensor.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+
+class Tensor(object):
+
+    def __init__(self, data):
+        self.data = np.array(data)
+
+    def __add__(self, other):
+        return Tensor(self.data + other.dat)
+
+    def __repr__(self):
+        return str(self.data.__repr__())
+
+    def __str__(self):
+        return str(self.data.__str__())
