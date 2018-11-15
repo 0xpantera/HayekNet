526d08ad52a178930ed2c5522250f37b03062aa8
Francososa <franky.sosa@gmail.com>
2018-11-15 00:37:31 -0600

    Substitute MSELoss by CrossEntropyLoss to test functionality

--- simple_nn.py
+++ simple_nn.py
@@ -1,16 +1,15 @@
 from tensor import Tensor, SGD, Sequential, Linear, MSELoss, Tanh, Sigmoid
-from tensor import Embedding
+from tensor import Embedding, CrossEntropyLoss
 import numpy as np
 np.random.seed(0)
 
 data = Tensor(np.array([1, 2, 1, 2]), autograd=True)
-target = Tensor(np.array([[0], [1], [0], [1]]), autograd=True)
+target = Tensor(np.array([0, 1, 0, 1]), autograd=True)
 
-embed = Embedding(5, 3)
-model = Sequential([embed, Tanh(), Linear(3, 1), Sigmoid()])
-criterion = MSELoss()
+model = Sequential([Embedding(3, 3), Tanh(), Linear(3, 4)])
+criterion = CrossEntropyLoss()
 
-optim = SGD(parameters=model.get_parameters(), alpha=0.5)
+optim = SGD(parameters=model.get_parameters(), alpha=0.1)
 
 for i in range(10):
 
